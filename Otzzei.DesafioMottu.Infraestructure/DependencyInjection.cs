8c27d928e84cb4e8eb482f890591b68915e25a23
lucas_ls <lucas.santos@fasters.com.br>
2025-11-08 10:43:35 -0300

    Adicionar arquivos de projeto.

---
+++ Otzzei.DesafioMottu.Infraestructure/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Otzzei.DesafioMottu.Infraestructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.DesafioMottu.Infraestructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<MottuDbContext>(options =>
+                options.UseNpgsql(config.GetConnectionString("MottuConnection")));
+
+            services.AddMassTransit(x =>
+            {
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(config["RabbitMq:Host"], h =>
+                    {
+                        h.Username(config["RabbitMq:Username"]);
+                        h.Password(config["RabbitMq:Password"]);
+                    });
+                });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Otzzei.DesafioMottu.Infraestructure/Persistence/MottuDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.DesafioMottu.Infraestructure.Persistence
+{
+    public class MottuDbContext : DbContext
+    {
+        public MottuDbContext(DbContextOptions<MottuDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MottuDbContext).Assembly);
+        }
+    }
+}
