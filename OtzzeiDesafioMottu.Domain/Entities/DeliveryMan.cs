55f06939d4368b6554174de0c689536e89dc27f8
lucas_ls <lucas.santos@fasters.com.br>
2025-11-09 12:19:35 -0300

    feat(deliveryman): create delivery man entity

---
+++ OtzzeiDesafioMottu.Domain/Entities/DeliveryMan.cs
@@ -0,0 +1,38 @@
+﻿using OtzzeiDesafioMottu.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OtzzeiDesafioMottu.Domain.Entities
+{
+    public class DeliveryMan
+    {
+        public Guid Id { get; private set; }
+        public string Identifier { get; private set; } = null!;
+        public string Name { get; private set; } = null!;
+        public string Cnpj { get; private set; } = null!;
+        public DateTime BirthDate { get; private set; }
+        public string CnhNumber { get; private set; } = null!;
+        public CNHTypeEnum CnhType { get; private set; }
+        public string? CnhImagePath { get; private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+
+        public DeliveryMan(string identifier, string name, string cnpj, DateTime birthDate, string cnhNumber, CNHTypeEnum cnhType)
+        {
+            Id = Guid.NewGuid();
+            Identifier = identifier;
+            Name = name;
+            Cnpj = cnpj;
+            BirthDate = birthDate;
+            CnhNumber = cnhNumber;
+            CnhType = cnhType;
+        }
+        public void UpdateCnhImage(string imagePath)
+        {
+            CnhImagePath = imagePath;
+        }
+    }
+}
