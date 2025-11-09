7556b916975d45299bc3081f3dddea7955b178cb
lucas_ls <lucas.santos@fasters.com.br>
2025-11-09 12:46:39 -0300

    add validators

---
+++ OtzzeiDesafioMottu.Domain/Validators/DeliveryManValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using OtzzeiDesafioMottu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtzzeiDesafioMottu.Domain.Validators
+{
+    public class DeliveryManValidator : AbstractValidator<DeliveryMan>
+    {
+        public DeliveryManValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Nome deve ser preenchido.");
+            RuleFor(x => x.Cnpj).NotEmpty().Length(14).WithMessage("CNPJ deve ter  14 digitos(apenas numeros).");
+            RuleFor(x => x.CnhNumber).NotEmpty().WithMessage("CNH deve ser preenchida");
+            RuleFor(x => x.CnhType).IsInEnum().WithMessage("Tipo de CNH invalido");
+        }
+    }
+}
---
+++ OtzzeiDesafioMottu.Domain/Validators/MotorcycleValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using OtzzeiDesafioMottu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtzzeiDesafioMottu.Domain.Validators
+{
+    public class MotorcycleValidator :AbstractValidator<Motorcycle>
+    {
+        public MotorcycleValidator()
+        {
+            RuleFor(x => x.Modelo).NotEmpty().WithMessage("Modelo da moto é obrigatorio.");
+            RuleFor(x => x.Ano).NotEmpty().WithMessage("Ano da moto é obrigatorio");
+            RuleFor(x => x.Placa).NotEmpty().WithMessage("Placa da moto é obrigatoria");
+        }
+    }
+}
